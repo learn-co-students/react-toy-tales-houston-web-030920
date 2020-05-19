9f11ace4e5109fb2c3f6acfa31a5b07dbf1ccace
willfbren <willfbren@gmail.com>
2020-05-18 20:28:35 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
 
-
-class App extends React.Component{
+class App extends React.Component {
 
   state = {
-    display: false
+    display: false,
+    toys: []
   }
 
   handleClick = () => {
@@ -21,20 +20,62 @@ class App extends React.Component{
     })
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(toyList => {
+      this.setState({
+        toys: toyList
+      })
+    })
+  }
+
+  handleSubmit = (toy) => {
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: toy.name,
+        image: toy.image,
+        likes: 0
+      })
+    })
+    .then(resp => resp.json())
+    .then(toy => {
+      this.setState({
+        toys: [...this.state.toys, toy]
+      })
+    })
+  }
+
+  handleDelete = (clickedToy) => {
+    fetch(`http://localhost:3000/toys/${clickedToy.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    this.setState({
+      toys: this.state.toys.filter(toy => toy.id !== clickedToy.id)
+    })
+  }  
+
   render(){
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm form={this.handleSubmit}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} deleteToy={this.handleDelete}/>
       </>
     );
   }
