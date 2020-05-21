3bf8b05759ddbbcc97d4ae38f35adb4acc9823dd
isaac-3 <isaacgc0596@gmail.com>
2020-05-21 18:34:13 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
-
-
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys: []
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/toys")
+    .then(res => res.json())
+    .then(toys => (
+      this.setState({
+        toys: toys
+      })
+    ))
   }
 
   handleClick = () => {
@@ -21,20 +29,47 @@ class App extends React.Component{
     })
   }
 
+  newToy = (toy) => {
+    fetch("http://localhost:3000/toys",{
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        name: toy.name,
+        image: toy.image,
+        likes: 0
+      })
+    })
+    .then(res => res.json())
+    .then(toy => (
+      this.setState({toys: [...this.state.toys, toy]})
+    ))
+  }
+
+  delToy = (toy) => {
+    fetch(`http://localhost:3000/toys/${toy.id}`,{
+      method: "DELETE",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        id: toy.id
+      })
+    })
+    let delToy = toy
+    this.setState({
+      toys: this.state.toys.filter(toy => (
+        toy != delToy
+      ))
+    })
+  }
+
   render(){
     return (
       <>
         <Header/>
-        { this.state.display
-            ?
-          <ToyForm/>
-            :
-          null
-        }
+        { this.state.display ? <ToyForm newToy={this.newToy}/> : null}
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} delToy={this.delToy}/>
       </>
     );
   }
